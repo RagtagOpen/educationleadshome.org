cadfa21ba758cdbc0fcde32f2e4a5e5386e8e370
Noah <noah.listgarten@menloschool.org>
2019-04-25 13:17:27 -0400

    Correct footer.php
    Actually correct footer grid by creating umbrella div

--- web/app/themes/coblocks-child/footer.php
+++ web/app/themes/coblocks-child/footer.php
@@ -19,38 +19,37 @@
 		<footer class="site-footer">
 
 			<?php get_sidebar(); ?>
-
-			<div class="site-info container center-align h6 medium smooth sans-serif-font gray" role="contentinfo">
-
-				<span class="site-title">
-					<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">&copy; <?php echo esc_html( date_i18n( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?></a>
-				</span>
-
-				<span class="ragtag-logo">
+			<div class="primary-footer">
+				<div class="site-info container center-align h6 medium smooth sans-serif-font gray" role="contentinfo">
+					<span class="site-title">
+						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">&copy; <?php echo esc_html( date_i18n( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?></a>
+					</span>
+				</div>
+			
+				<div class="ragtag-logo">
 					<a href="https://ragtag.org/?utm_medium=web&amp;utm_source=education-leads-home&amp;utm_name=empowered-badge">
 					<img src="https://assets.ragtag.tech/empowered-by-ragtag.svg" alt="Empowered by Ragtag">
 					</a>
-				</span>
+				</div>
 
+				<div class="footer-nav">
+					<?php if ( has_nav_menu( 'footer' ) ) : ?>
+						<nav class="footer-navigation container" aria-label="<?php esc_attr_e( 'Footer Menu', '@@textdomain' ); ?>">
+
+							<?php
+							wp_nav_menu(
+								array(
+									'theme_location' => 'footer',
+									'menu_class'     => 'footer-menu sans-serif-font medium gray smooth h6 list-reset',
+									'depth'          => '1',
+								)
+							);
+							?>
+						</nav>
+					<?php endif; ?>
+				</div>
 			</div>
 
-			<?php if ( has_nav_menu( 'footer' ) ) : ?>
-
-				<nav class="footer-navigation container" aria-label="<?php esc_attr_e( 'Footer Menu', '@@textdomain' ); ?>">
-
-					<?php
-					wp_nav_menu(
-						array(
-							'theme_location' => 'footer',
-							'menu_class'     => 'footer-menu sans-serif-font medium gray smooth h6 list-reset',
-							'depth'          => '1',
-						)
-					);
-					?>
-
-				</nav>
-
-			<?php endif; ?>
 
 		</footer>
 
@@ -61,6 +60,5 @@
 	<?php wp_footer(); ?>
 
 	</body>
-
 
 </html>
