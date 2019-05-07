aeb612b415faff2bc5c1b977b8cf3d8fc533ce26
Noah <noah.listgarten@menloschool.org>
2019-05-07 15:15:25 -0400

    Update footer.php with sponsor logos
    Added sponsor logos to the footer above the privacy policy, logo, and footer nav

--- web/app/themes/coblocks-child/footer.php
+++ web/app/themes/coblocks-child/footer.php
@@ -20,6 +20,22 @@
 
 			<?php get_sidebar(); ?>
 			<div class="primary-footer">
+				
+				<div class="sponsor-logos">
+					<a href="https://www.schoolhouseconnection.org" target="_blank">
+						<img src="https://i0.wp.com/s3.amazonaws.com/educationleadshome-org/app/uploads/2019/04/03033013/schoolhouse-bubble.png?zoom=2&w=171&h=171&crop=1&ssl=1" alt="SchoolHouse Connection">
+					</a>
+					<a href="https://www.americaspromise.org" target="_blank">
+						<img src="https://i2.wp.com/s3.amazonaws.com/educationleadshome-org/app/uploads/2019/04/03033009/apa-bubble.png?zoom=2&w=171&h=171&crop=1&ssl=1" alt="America's Promise Alliance">
+					</a>
+					<a href="https://www.civicllc.com" target="_blank">
+						<img src="https://i1.wp.com/s3.amazonaws.com/educationleadshome-org/app/uploads/2019/04/03033010/civic-bubble.png?zoom=2&w=171&h=171&crop=1&ssl=1" alt="Civic Enterprises">
+					</a>
+					<a href="https://www.icphusa.org" target="_blank">
+						<img src="https://i1.wp.com/s3.amazonaws.com/educationleadshome-org/app/uploads/2019/04/03033012/icph-bubble.png?zoom=2&w=171&h=171&crop=1&ssl=1" alt="Institute for Children, Poverty and Homelessness">
+					</a>
+				</div>
+				
 				<div class="site-info container center-align h6 medium smooth sans-serif-font gray" role="contentinfo">
 					<span class="site-title">
 						<a href="<?php echo esc_url( home_url( '/' ) ); ?>" rel="home">&copy; <?php echo esc_html( date_i18n( 'Y' ) ); ?> <?php bloginfo( 'name' ); ?></a>
