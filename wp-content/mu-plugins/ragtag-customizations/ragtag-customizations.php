34dee2942b1b21d235de6debc960b8ea1e4e7424
dryan <dryan@users.noreply.github.com>
2019-08-30 10:57:24 -0400

    plugin to insert mailchimp js into <head>

---
+++ wp-content/mu-plugins/ragtag-customizations/ragtag-customizations.php
@@ -0,0 +1,15 @@
+<?php
+/*
+Plugin name: Ragtag Customizations
+Plugin URI: https://ragtag.org/
+Description: This plugin adds customizations to the current theme.
+Author: Ragtag
+Author URI: https://ragtag.org/
+Version: 1.0
+*/
+
+add_action( 'wp_head', 'ragtag_wp_head_customizations' );
+
+function ragtag_wp_head_customizations() {
+  echo('<script id="mcjs">!function(c,h,i,m,p){m=c.createElement(h),p=c.getElementsByTagName(h)[0],m.async=1,m.src=i,p.parentNode.insertBefore(m,p)}(document,"script","https://chimpstatic.com/mcjs-connected/js/users/c12853a51b1bfe0d4a53a760e/28e040a3fbe168f8d57c8a62a.js");</script>');
+}
